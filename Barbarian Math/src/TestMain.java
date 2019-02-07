1fc65030a0bc73558faccc3a194f4239de30a0db
Kyle <kbcoulombe@gmail.com>
2019-02-07 09:07:04 -0500

    Update TestMain.java

--- Barbarian Math/src/TestMain.java
+++ Barbarian Math/src/TestMain.java
@@ -5,6 +5,7 @@ public class TestMain {
 	public static void main(String[] args) {
 		DisplayMain maths = new DisplayMain();
 		maths.show();
+		Ruleset DnD_5e = new Ruleset("BarbarianMathData/DnD_5e");
 	}
 
 }
