5ef5e69cf850c7f844747146a15c20f2498077b4
Kyle <kbcoulombe@gmail.com>
2019-03-06 14:06:35 -0500

    Update TestMain.java

--- Barbarian Math/src/TestMain.java
+++ Barbarian Math/src/TestMain.java
@@ -1,14 +1,38 @@
+import java.io.File;
+
 import javax.xml.parsers.ParserConfigurationException;
 
 import gui.DisplayMain;
-import logic.Ruleset;
+import logic.*;
 
 public class TestMain {
 
-	public static void main(String[] args) throws ParserConfigurationException {
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException 
+	{
 		DisplayMain maths = new DisplayMain();
 		maths.show();
-		Ruleset DnD_5e = new Ruleset("BarbarianMathData/DnD_5e");
+		
+		File workingDirectory = new File("BarbarianMathData");
+		Scanner scanner = new Scanner();
+		
+		File[] rulesets = scanner.ListRulesets(workingDirectory);
+		
+		Ruleset DnD_5e = null;
+		for(int i = 0; i < rulesets.length; i++)
+		{
+			if(rulesets[i].getName().equals("DnD_5e"))
+			{
+				DnD_5e = scanner.LoadRuleset(rulesets[i]);
+				break;
+			}
+		}
+		
+		System.out.println(DnD_5e.name);
+		
+		System.out.println(scanner.FindComponentInXML("Dagger", "BarbarianMathData/DnD_5e/Modifiers_Weapons.val", "modifier").getElementsByTagName("tags").item(0).getTextContent());
+		
+		Modifier testModifier = new Modifier("Dagger", null, null, null, null);
+		scanner.Write(testModifier, "BarbarianMathData/DnD_5e/Modifiers_Weapons.val");
 	}
 
 }
