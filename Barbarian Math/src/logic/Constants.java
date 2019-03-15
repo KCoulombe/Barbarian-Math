043cdc3517fdc2819641e229ec820df291dd6f79
tmckulla <47018254+tmckulla@users.noreply.github.com>
2019-03-15 13:59:43 -0400

    Add files via upload
    Reorganized Constants.

--- Barbarian Math/src/logic/Constants.java
+++ Barbarian Math/src/logic/Constants.java
@@ -27,6 +27,12 @@ public class Constants {
 	public static final String GOTO_SAVE_BUTTON = "GOTO_SAVE_BUTTON";
 	public static final String GOTO_LOAD_BUTTON = "GOTO_LOAD_BUTTON";
 	public static final String GOTO_MAIN_BUTTON = "GOTO_MAIN_BUTTON";
+	public static final String ENTER_MODIFIER_LOAD = "ENTER_MODIFIER_LOAD";
+	public static final String ENTER_LIMIT = "ENTER_LIMIT";
+	public static final String GOTO_FIRST_MAIN_BUTTON = "GOTO_FIRST_MAIN_BUTTON";
+	public static final String ENTER_TYPE = "ENTER_TYPE";
+	public static final String ENTER_COST = "ENTER_COST";
+	
 	//labels for buttons
 	public static final String GOTO_MAIN_BUTTON_LABEL = "Start";
 	public static final String GOTO_CALCULATOR_BUTTON_LABEL = "Calculate";
@@ -34,41 +40,48 @@ public class Constants {
 	public static final String GOTO_MODIFIER_BUTTON_LABEL = "Modifier Manager";
 	public static final String GOTO_SAVE_BUTTON_LABEL = "Save";
 	public static final String GOTO_LOAD_BUTTON_LABEL = "Load";
+	public static final String LABEL_MODIFIER_LOAD = "Load Modifier";
+	public static final String LABEL_MODIFIER_LOAD_BUTTON = "Load";
+	public static final String LABEL_NAME_FIELD = "Name:";
+	public static final String LABEL_ATTRIBUTE_SELECT = "Attribute: ";
+	public static final String LABEL_COST_FIELD = "Cost Amount";
+	public static final String LABEL_ADD_BUTTON = "Add";
+	public static final String LABEL_TYPE_DROP = "Type: ";
+	public static final String LABEL_LIMIT_DROP = "Limit: ";
+	public static final String LABEL_LOAD_MOD_ROW = "Mod Loaded";
+	public static final String LABEL_BACK_BUTTON = "Back";
 	
 	public static final String DATA_FOLDER_NAME = "./BarbarianMathData";
 	public static final String EXTENTION_RULESET = ".ruleset";
-	public static final String LABEL_MODIFIER_LOAD = "Load Modifier";
 	
 	
+	//Tags
 	public static final String TAG_MODIFIER = "TAG_MODIFIER";
-	
-	public static final String LABEL_MODIFIER_LOAD_BUTTON = "Load";
-	public static final String ENTER_MODIFIER_LOAD = "ENTER_MODIFIER_LOAD";
-	public static final String LABEL_NAME_FIELD = "Name:";
+	public static final String TAG_LIMIT = "TAG_LIMIT";
 	public static final String TAG_NAME = "TAG_NAME";
 	public static final String TAG_ATTRIBUTE = "TAG_ATTRIBUTE";
-	public static final String LABEL_ATTRIBUTE_SELECT = "Attribute Used";
 	public static final String TAG_INTEGER = "INTEGER";
-	public static final String LABEL_COST_FIELD = "Cost Amount";
-	public static final String ENTER_COST = "ENTER_COST";
-	public static final String LABEL_ADD_BUTTON = "Add";
-	public static final String LABEL_TYPE_DROP = "Type: ";
-	public static final String ENTER_TYPE = "ENTER_TYPE";
+	
 	
 	// start Tags from classes and other modifiers
-	public static final String TAG_WEAPON_FINESSE = "Weapon Finesse";
-	public static final String TAG_CLASS = "class";
-	public static final String TAG_REACTION = "reaction";
-	public static final String TAG_MAGIC = "magic";
-	public static final String TAG_SPELL = "spell";
-	public static final String TAG_SCALING = "scaling";
-	public static final String LIMIT_ONCE_PER_TURN = "oncePerTurn";
-	public static final String LIMIT_ON_HIT = "onHit":
-	public static final String TAG_WIS_MOD ="wisMod";
-	public static final String TAG_DEX_MOD ="dexMod";
-	public static final String TAG_LIGHTNING = "lightning";
-	public static final String TAG_ACID ="acid";
-	public static final String TAG_SPELL_SLOT="spellslot";
-	public static final String TAG_FULL_CASTER = "fullCaster";
+		public static final String TAG_WEAPON_FINESSE = "Weapon Finesse";
+		public static final String TAG_CLASS = "class";
+		public static final String TAG_REACTION = "reaction";
+		public static final String TAG_MAGIC = "magic";
+		public static final String TAG_SPELL = "spell";
+		public static final String TAG_SCALING = "scaling";
+		public static final String LIMIT_ONCE_PER_TURN = "oncePerTurn";
+		public static final String LIMIT_ON_HIT = "onHit";
+		public static final String TAG_WIS_MOD ="wisMod";
+		public static final String TAG_DEX_MOD ="dexMod";
+		public static final String TAG_LIGHTNING = "lightning";
+		public static final String TAG_ACID ="acid";
+		public static final String TAG_SPELL_SLOT="spellslot";
+		public static final String TAG_FULL_CASTER = "fullCaster";
 	//End Tags from classes and other modifiers
+	
+	
+	
+	
+
 }
