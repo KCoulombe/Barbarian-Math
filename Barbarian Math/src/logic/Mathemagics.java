94b18a4769a975c73afea7fbe65d8534c09a4fa9
tmckulla <47018254+tmckulla@users.noreply.github.com>
2019-03-29 14:02:08 -0400

    Add files via upload
    The real math object

---
+++ Barbarian Math/src/logic/Mathemagics.java
@@ -0,0 +1,24 @@
+package logic;
+
+import java.util.ArrayList;
+
+/**
+ * Does the math, all the math. 
+ * @author Tim
+ *
+ */
+public class Mathemagics {
+
+	public Mathemagics() {
+	}
+	/**
+	 * Outward facing method. should be only visible method
+	 * @param builds The CharacterBuilds to be parsed
+	 * @return
+	 */
+	public static Record makeRecords(ArrayList<CharacterBuild> builds)
+	{
+		return null;
+	}
+
+}
